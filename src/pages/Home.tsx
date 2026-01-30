d52110efce702e5e090d9176dd32165160bfc2ca
winchester <abhirak.ch@gmail.com>
2026-01-30 17:32:53 +0700

    feat: add tailwindcss for the style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
 export default function Home() {
   return (
     <>
+      <h1 className="text-3xl font-bold underline text-center text-blue-600">
+        Hello world!
+      </h1>
       <p>Home</p>
     </>
   );
